46032a90a3f7a3702f934fd17ecfdc3f9586f36d
Mario Martinez <mariom@thinkful.com>
2020-03-13 15:35:46 -0500

    fixing app

--- react-inventory-ease-client-34cd89c6fffcf77da19fc3c73ea74a3d5e1ec901/src/components/inventory-ease.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {shallow, mount} from 'enzyme';
-import InventoryEase from './inventory-ease';
-
-describe('<InventoryEase />', () => {
-    it('Renders without crashing', () => {
-        shallow(<InventoryEase />);
-    });
-});
