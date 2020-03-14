15e06945584c647b6f236a23f8e13586dd74aedd
Mario Martinez <mariom@thinkful.com>
2020-03-13 21:46:06 -0500

    change background image size

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -18,6 +18,10 @@ export default function LandingPage() {
             
             <header role="banner">
                 <h2 className="landingTitle">Streamline your auto dealerships inventory by personnel tracking: YEAR, MAKE, MODEL, MILEAGE, PARKING LOCATION AND deleting vehicle info.</h2>
+                
+                
+                
+                
                 <h2>Tracking vehicles will decrease lag time.</h2>
                 <h2>Less waiting!!</h2> 
             </header>
